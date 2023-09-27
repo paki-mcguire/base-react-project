35b247b9beede318cab1f4ac2d12c315f9e6c13b
Francis McGuire <f.mcguire@paki-logistics.com>
2023-09-27 11:17:39 +0200

    vitest implemented and validated

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default ({
   plugins: [react()],
+  test: {
+    reporters: ['html'],
+    coverage: {
+      provider: 'v8' // or 'istanbul'
+    },
+  }
 })
